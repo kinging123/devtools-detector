f86b7aeb3d522164ae5847576ced39df24d456c4
Reuven Karasik <rubik@karasik.org>
2020-03-22 14:47:57 +0200

    Added disableDevToolsDetection modifer

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
 (function() {
-    function detectDevTool(allow) {
-        if (isNaN(+allow)) {
-            allow = 100;
-        }
-        var start = +new Date();
-        debugger;
-        var end = +new Date();
-        if (isNaN(start) || isNaN(end) || end - start > allow) {
-            window.dispatchEvent(
-              new CustomEvent("devtoolschange", {
+    function fireEvent(isOpen) {
+        window.dispatchEvent(
+            new CustomEvent("devtoolschange", {
                 detail: {
                     isOpen: isOpen
                 }
-              })
+            })
             );
         }
-    }
-    if (window.attachEvent) {
-        if (
-            document.readyState === "complete" ||
-            document.readyState === "interactive"
-            ) {
-                detectDevTool();
-                window.attachEvent("onresize", detectDevTool);
-                window.attachEvent("onmousemove", detectDevTool);
-                window.attachEvent("onfocus", detectDevTool);
-                window.attachEvent("onblur", detectDevTool);
-            } else {
-                setTimeout(argument.callee, 0);
+        function detectDevTool(allow) {
+            if (window.disableDevToolsDetection) {
+                return;
             }
-    } else {
-        window.addEventListener("load", detectDevTool);
-        window.addEventListener("resize", detectDevTool);
-        window.addEventListener("mousemove", detectDevTool);
-        window.addEventListener("focus", detectDevTool);
-        window.addEventListener("blur", detectDevTool);
-    }
-})();
+            if (isNaN(+allow)) {
+                allow = 100;
+            }
+            var start = +new Date();
+            debugger;
+            var end = +new Date();
+            if (isNaN(start) || isNaN(end) || end - start > allow) {
+                fireEvent(true);
+            } else {
+                fireEvent(false);
+            }
+        }
+        if (window.attachEvent) {
+            if (
+                document.readyState === "complete" ||
+                document.readyState === "interactive"
+                ) {
+                    detectDevTool();
+                    window.attachEvent("onresize", detectDevTool);
+                    window.attachEvent("onmousemove", detectDevTool);
+                    window.attachEvent("onfocus", detectDevTool);
+                    window.attachEvent("onblur", detectDevTool);
+                } else {
+                    setTimeout(argument.callee, 0);
+                }
+            } else {
+                window.addEventListener("resize", detectDevTool);
+                window.addEventListener("mousemove", detectDevTool);
+            }
+        })();
