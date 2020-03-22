261edd77e11fd3d3f46ace6405c961bd85274b43
Reuven Karasik <rubik@karasik.org>
2020-03-22 14:50:19 +0200

    fixed indentation

--- main.js
+++ main.js
@@ -9,39 +9,39 @@
                     isOpen: isOpen
                 }
             })
-            );
+        );
+    }
+    function detectDevTool(allow) {
+        if (window.disableDevToolsDetection) {
+            return;
         }
-        function detectDevTool(allow) {
-            if (window.disableDevToolsDetection) {
-                return;
-            }
-            if (isNaN(+allow)) {
-                allow = 100;
-            }
-            var start = +new Date();
-            debugger;
-            var end = +new Date();
-            if (isNaN(start) || isNaN(end) || end - start > allow) {
-                fireEvent(true);
-            } else {
-                fireEvent(false);
-            }
+        if (isNaN(+allow)) {
+            allow = 100;
         }
-        if (window.attachEvent) {
-            if (
-                document.readyState === "complete" ||
-                document.readyState === "interactive"
-                ) {
-                    detectDevTool();
-                    window.attachEvent("onresize", detectDevTool);
-                    window.attachEvent("onmousemove", detectDevTool);
-                    window.attachEvent("onfocus", detectDevTool);
-                    window.attachEvent("onblur", detectDevTool);
-                } else {
-                    setTimeout(argument.callee, 0);
-                }
-            } else {
-                window.addEventListener("resize", detectDevTool);
-                window.addEventListener("mousemove", detectDevTool);
-            }
-        })();
+        var start = +new Date();
+        debugger;
+        var end = +new Date();
+        if (isNaN(start) || isNaN(end) || end - start > allow) {
+            fireEvent(true);
+        } else {
+            fireEvent(false);
+        }
+    }
+    if (window.attachEvent) {
+        if (
+            document.readyState === "complete" ||
+            document.readyState === "interactive"
+        ) {
+            detectDevTool();
+            window.attachEvent("onresize", detectDevTool);
+            window.attachEvent("onmousemove", detectDevTool);
+            window.attachEvent("onfocus", detectDevTool);
+            window.attachEvent("onblur", detectDevTool);
+        } else {
+            setTimeout(argument.callee, 0);
+        }
+    } else {
+        window.addEventListener("resize", detectDevTool);
+        window.addEventListener("mousemove", detectDevTool);
+    }
+})();
